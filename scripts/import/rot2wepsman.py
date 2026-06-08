8322c5ab7986b3f19cba4ad6f4e895d8adc36892
akrherz <akrherz@iastate.edu>
2026-06-08 13:16:03 -0500

    🐛 Fix year output in rot2weps

--- scripts/import/rot2wepsman.py
+++ scripts/import/rot2wepsman.py
@@ -65,7 +65,8 @@ def do_flowpath(
                 except KeyError:
                     continue
             outfh.write(
-                f"D {int(day):02.0f}/{int(month):02.0f}/{int(year):02.0f}\n"
+                f"D {int(day):02.0f}/{int(month):02.0f}/"
+                f"{int(year) + 2006:02.0f}\n"
             )
             outfh.write(payload + "\n#----\n")
         outfh.write("*END\n*EOF")
